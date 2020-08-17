f2bd7362139993646a5f65cbe075737ecc895397
JohnTrump <wujunchuan2008@gmail.com>
2020-08-18 00:09:53 +0800

    feat: 安全模块

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+
 // import * as passport from 'passport';
 // import * as session from 'express-session';
 
@@ -26,8 +29,27 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
 
+  /* 设置接口请求频率 */
+  // 访问频率限制
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15分钟
+      max: 1000, // 限制15分钟内最多只能访问1000次
+    }),
+  );
+  /* 网络安全 - Web漏洞 */
+  app.use(helmet());
   /* 统一验证DTO */
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      skipMissingProperties: false,
+      forbidUnknownValues: true,
+    }),
+  );
+
   /* 统一请求成功的返回数据 */
   app.useGlobalInterceptors(new TransformInterceptor());
   /* 拦截全部的错误请求,统一返回格式 */
